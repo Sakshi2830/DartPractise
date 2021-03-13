231f2e6a4d32fb36cf2e1f3a912d65744394e4c0
Sakshi Tripathi <inboxtosakshi@gmal.com>
2021-03-13 13:18:46 +0530

    class commit

--- class.dart
+++ class.dart
@@ -1,5 +1,30 @@
-class Employee{
-  final String name;
-  Employee(this.name);
-  // constructor
+void main(){
+final person=Person(name:'Sakshi',age: 21,height: 1.64);
+//final person=Person();
+//person.name='bob';
+print(person.describe());
+// print(person.describe2());
+final employee=Employee(name:'Sakshi',age: 21,height: 1.64,taxcode:'Sakshi',salary:50000);
+print(employee.describe());
+
+
+// ctrl + space 
+// final person12 = Person();
+}
+class Person{
+  Person({this.name='vscode', this.age=0, this.height=0.0});//constructer
+   String name;
+  final int age;
+  final double height;
+  String describe(){ //by using [] brackets its means it give null value where ever it is used ...nad also pass some sensible default value to make is useable..
+     return"my name is $name . my age is $age . my hieght is $height meters";
+  }
+  
+  void describe2() =>"my name is $name . my age is $age . my hieght is $height meters"; 
+}
+class Employee extends Person{
+  final String taxcode;
+  final int salary;
+  Employee({String name='null', int age=0, double height=0.0,this.taxcode='vcode', this.salary=0});
+  // String dtest()=>('new $taxcode ,new $salary'); 
 }
